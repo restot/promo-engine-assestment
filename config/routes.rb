f2f93313b41b8c60141175b9e7bf3426062c9f87
Illia Barkov <92ilya.icom@gmail.com>
2022-08-22 22:25:38 -0400

    refining routes.rb  22-08-22 22:25:38

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,18 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+
   get 'promo/assing_product'
   get 'promo/unassing_product'
   get 'promo/index'
+
   get 'cart/index'
   get 'cart/add_to_cart'
   get 'cart/remove_from_cart'
-  get 'products/seed'
-  get 'products/resead'
-  get 'products/show'
-  get 'products/edit'
-  get 'products/destroy'
-  get 'products/create'
-  get 'products/index'
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources 'products' do
+    get 'products/seed'
+    get 'products/resead'
+  end
+
 end
