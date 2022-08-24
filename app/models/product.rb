75eeebfd28886a69970f4c13ac87890cc7c9e302
Illia Barkov <92ilya.icom@gmail.com>
2022-08-24 01:54:38 -0400

    added fixed price for set promo logic  22-08-24 01:54:38

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   belongs_to :cart
+  has_many :promos
 end
--- app/models/promo.rb
+++ app/models/promo.rb
@@ -1,12 +1,19 @@
 class Promo < ApplicationRecord
-  belongs_to :product_id
+  has_many :product_id
 
-  def fixed_price_for_set
-
+  def fixed_price_for_set(product, count)
+    promo = Promo.find_by(product_id: product.id, promo_type: "fixed_price")
+    if promo.nil?
+      nil # return nil if no promos has been found for product
+    elsif count >= promo.promo_trigger
+      promo.price # return price for set of items if conditions are met
+    else
+      false # return false if product eligible for promotion but conditions haven't been met
+    end
   end
 
-  def fixed_price_for_match
-
+  def fixed_price_for_match(product, current_cart)
+    promo = Promo.find_by(product_id: product.id, promo_type: "fixed_price")
   end
 
 end
